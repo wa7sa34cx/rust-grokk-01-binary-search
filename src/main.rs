39ae9d02d70c63fc93832115282af7240fc7659f
Alex Black <wa7@sa34.cx>
2021-04-10 17:15:41 +0300

    End

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    // searchable list
+    let list = [1, 3, 5, 7, 9, 11, 13, 18, 19, 20, 21, 31, 55, 70, 82, 99];
+    let item = 70;
+
+    // println!("The index of {} is {}", item, binary_search(list, item));
+
+    let mut low = 0;
+    let mut high = list.len() - 1;
+    // let mut mid = (low + high) / 2;
+
+    // println!("{}", high);
+
+    while low <= high {
+        let mid = (low + high) / 2;
+
+        match list[mid].cmp(&item) {
+            Ordering::Less => {
+                low = mid + 1;
+            },
+            Ordering::Greater => {
+                high = mid - 1;
+            },
+            Ordering::Equal => {
+                println!("The index of {} is {}", item, mid);
+                break;
+            }
+        }
+    }
+
 }
